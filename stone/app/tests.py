5c7689ff66d0bd2d27e3a343463b1c2d5f330f91
danitlf <danilimah18@gmail.com>
2017-05-21 21:59:11 -0300

    time e outras features

--- stone/app/tests.py
+++ stone/app/tests.py
@@ -4,3 +4,22 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+class HomeTest(TestCase):
+    def setUp(self):
+	    self.resp = self.client.get('/')
+    def test_get(self):
+	    #get / must return 200
+	    self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):		
+	    self.assertTemplateUsed(self.resp, 'index.html')
+        
+class EnviaEmailTest(TestCase):
+    def setUp(self):
+	    self.resp = self.client.get('/mensagem_enviada/')
+
+    def test_get_send_email_url(self):
+        #get / must return 200
+	    self.assertEqual(200, self.resp.status_code)
+    def test_template(self):		
+	    self.assertTemplateUsed(self.resp, 'index.html')
